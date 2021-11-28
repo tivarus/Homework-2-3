3ba26003a7c9690cf798946f54ef202efa4b1836
tivarus <53016303+tivarus@users.noreply.github.com>
2021-11-28 08:17:05 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,95 +1,143 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-int main(){
+
+int Sort(int* array, int numbers, int ribs) {
+
+    int i, n, j, temp[numbers];
+    for (int i = 0; i < numbers; i++) {
+        temp[i] = array[i];
+    }
+    for (i = 0; i < numbers - 1; i++) {
+       for (j = 0; j < numbers - i - 1; j++) {
+           if(temp[j] > temp[j+1]) {
+              int n = temp[j];
+              temp[j] = temp[j+1];
+              temp[j+1] = n;
+           }
+       }
+    }
+
+    int duplicates = 0;
+    for (int i = 1; i < numbers; i++) {
+        if (temp[i] == temp[i-1]) {
+            ++duplicates;
+        }
+    }
+    int count = numbers - duplicates - ribs;
+    return count;
+}
+
+int SearchCycle (int numbers, int* array, int a, int x, int size, int* check, int start, int h){
+    int error;
+    while (a < numbers) {
+        if (start == array[a] && a != h) {
+
+            if (a % 2 != 0) {
+                --a;
+            } else {
+                ++a;
+            }
+            x = array[a];
+
+            for (int i = 0; i < size+1; i++) {
+                if(x == check[i]) {
+                    error = 1;
+                }
+            }
+
+            if (error != 1) {
+                ++size;
+                check[size] = x;
+                h = a;
+                SearchCycle(numbers, array, 0, x, size, check, check[size], h);
+                if (SearchCycle(numbers, array, 0, x, size, check, check[size] , h) == 1) {
+                    return 1;
+                }
+                a +=2;
+            } else {
+                return 1;
+            }
+
+        }
+        else{
+            ++a;
+        }
+    }
+    return 0;
+}
+
+int main() {
     FILE* file_graph;
     FILE* result;
-    char s, f;
-    int n, u, v, m, g, k = 0, z = 0, q = 0;
+    char symbol;
+    int ribs = 0, numbers = 0, first, second, dash = 0, Loop = 0, Multiple = 0, SingleDot = 0, x, size = 0, a = 0, start;
 
-    //подсчет количества ребер
     file_graph = fopen("graph.txt", "r");
-    while ((s = getc(file_graph)) != EOF) {
-        putc(s, stdin);
-        if (s == '\n')
-            n ++;
+    while ((symbol = getc(file_graph)) != EOF) {
+        if (symbol == '\n') {
+            ribs ++;
+        }
+        if (symbol == '-') {
+            dash ++;
+        }
     }
-    ++n;
+    ++ribs;
+    if (dash != ribs) {
+        SingleDot = 1;
+    }
     fclose(file_graph);
 
-    //запись графа в двумерный массив
-    int edges[n][2];
+    numbers = ribs * 2;
+    int array[numbers];
     file_graph = fopen("graph.txt", "r");
-    for (int i = 0; i < n; i++) {
-        fscanf(file_graph, "%d%c%d;", &u, &f, &v);
-        if (f == ';')
-            k = 1;
-        edges[i][0] = u;
-        edges[i][1] = v;
+    for (int i = 0; i < numbers; i += 2) {
+        fscanf(file_graph, "%d-%d", &first, &second);
+        array[i] = first;
+        array[i+1] = second;
     }
     fclose(file_graph);
-    m = n * 2;//кол-во записанных цифр в файле
-    int a[m];
-    int i, b;
 
-    //проверка на петли
-    for(int i = 0; i < n; i++) {
-        if(edges[i][0] == edges[i][1])
-            z = 1;
-    }
-
-    //проверка на кратность
-    for(int i = 0; i < n; i++) {
-        for( int b = i + 1; b < n; b++){
-            if((edges[i][0] == edges[b][0] && edges[i][1] == edges[b][1]) || (edges[i][1] == edges[b][0] && edges[i][0] == edges[b][1]))
-                q = 1;
+    for(int i = 0; i < numbers; i += 2) {
+        if(array[i] == array[i+1]) {
+            Loop = 1;
         }
     }
 
-    //подсчет количества вершин
-    while(i != n + 1){//запись графа в одномерный массив
-        a[b]=edges[i][0];
-        ++b;
-        a[b]=edges[i][1];
-        ++b;
-        ++i;
-    }
-
-    bool p;
-    for (int i = m - 1; i >= 0; i--) {//сортировка массива
-        p = 1;
-        for (int j = 0; j < i; j++) {
-            if (a[j] > a[j + 1]) {
-                g = a[j];
-                a[j] = a[j + 1];
-                a[j + 1] = g;
-                p = 0;
+    for (int i = 0; i < numbers; i += 2) {
+        for ( int j = i + 2; j < numbers; j += 2){
+            if ((array[i] == array[j] && array[i+1] == array[j+1]) || (array[i] == array[j+1] && array[i+1] == array[j])){
+                Multiple = 1;
             }
         }
-        if (p == 1)
-            break;
     }
-    int c = 0;
-    for (int i = 1; i < m; i++) {//кол-во повтор€ющихс€ цифр
-        if (a[i] == a[i-1])
-            ++c;
-    }
-    int d = m - c - n; //всего цифр минус кол-во повтор€ющихс€ цифр минус число ребер, если !=1 то граф не дерево
 
-    //вывод результата
-    if (z == 0 && q == 0 && k == 0 && d == 1){
-        puts("Graph is a tree\n");
-        result = fopen("result.txt", "w");
+    int check[numbers];
+    check[size] = start = array[0];
+    int h = array[1];
+    int Cycle = SearchCycle(numbers, array, a, x, size, check, start, h);
+
+    int count = Sort(array, numbers, ribs);
+
+    printf("SingleDot is %d\n", SingleDot);
+    printf("count is %d\n", count);
+    printf("cycle is %d\n", Cycle);
+    printf("Loop is %d\n", Loop);
+    printf("Multiple is %d\n", Multiple);
+    result = fopen("result.txt", "w");
+    if (Loop == 0 && Multiple == 0 && Cycle != 1 && count == 1 && SingleDot == 0){
+        printf("Graph is a tree\n");
+
         fprintf(result, "graph {\n");
-        for(int i = 0; i < n; i++) //запись графа в файл result на €зыке dot, если он €вл€етс€ деревом
-            fprintf(result, "   %d -- %d;\n",edges[i][0], edges[i][1]);
+        for (int i = 0; i < numbers; i=i+2) {
+            fprintf(result, "   %d -- %d;\n",array[i], array[i+1]);
+        }
         fprintf(result, "}");
-        fclose(result);
     } else {
-        puts("graph is NOT a tree\n");
-        result = fopen("result.txt", "w");
-        fprintf(result, "Graph is NOT a tree\n");
-        fclose(result);
+    printf("Graph is NOT a tree\n");
+
+    fprintf(result, "The last graph was NOT a tree\n");
     }
+    fclose(result);
     return 0;
 }
