d477c88f5dccf6de7cdb3959490eb5cd491bd16f
tivarus <53016303+tivarus@users.noreply.github.com>
2021-12-17 11:56:06 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,62 +1,45 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <stdbool.h>
+#include <stdlib.h>
 
-int Sort(int* array, int numbers, int ribs) {
-
-    int i, n, j, temp[numbers];
+void Sort(int* array, int numbers, int* temp) {
+    
     for (int i = 0; i < numbers; i++) {
         temp[i] = array[i];
     }
-    for (i = 0; i < numbers - 1; i++) {
-       for (j = 0; j < numbers - i - 1; j++) {
-           if(temp[j] > temp[j+1]) {
-              int n = temp[j];
-              temp[j] = temp[j+1];
-              temp[j+1] = n;
-           }
-       }
-    }
-
-    int duplicates = 0;
-    for (int i = 1; i < numbers; i++) {
-        if (temp[i] == temp[i-1]) {
-            ++duplicates;
+    for (int i = 0; i < numbers - 1; i++) {
+        for (int j = 0; j < numbers - i - 1; j++) {
+            if (temp[j] > temp[j + 1]) {
+                int n = temp[j];
+                temp[j] = temp[j + 1];
+                temp[j + 1] = n;
+            }
         }
     }
-    int count = numbers - duplicates - ribs;
-    return count;
 }
 
-int SearchCycle (int numbers, int* array, int a, int x, int size, int* check, int start, int h){
-    int error;
-    for(int a = 0; a < numbers; ++a) {
+int SearchCycle(int numbers, int* array, int size, int* check, int start, int h) {
+    int x;
+    for (int a = 0; a < numbers; ++a) {
         if (start == array[a] && a != h) {
-
             if (a % 2 != 0) {
-                --a;
+                x = array[a-1];
+                h = a-1;
             } else {
                 ++a;
-            }
-            x = array[a];
-
-            for (int i = 0; i < size+1; i++) {
-                if(x == check[i]) {
-                    error = 1;
-                }
-            }
-
-            if (error != 1) {
-                ++size;
-                check[size] = x;
+                x = array[a];
                 h = a;
-                SearchCycle(numbers, array, 0, x, size, check, check[size], h);
-                if (SearchCycle(numbers, array, 0, x, size, check, check[size] , h) == 1) {
+            }
+
+            for (int i = 0; i < size + 1; i++) {
+                if (x == check[i]) {
                     return 1;
                 }
-                if (a % 2 == 0) {
-                    ++a;
-                }
-            } else {
+            }
+
+            ++size;
+            check[size] = x;
+            if (SearchCycle(numbers, array, size, check, x, h) == 1) {
                 return 1;
             }
         }
@@ -64,70 +47,82 @@ int SearchCycle (int numbers, int* array, int a, int x, int size, int* check, in
     return 0;
 }
 
-int main() {
+int main(void) {
     FILE* file_graph;
     FILE* result;
     char symbol;
-    int ribs = 0, numbers = 0, first, second, Loop = 0, Multiple = 0, x, size = 0, a = 0, start;
+    int numbers = 0, size = 0, start;
 
     file_graph = fopen("graph.txt", "r");
     while ((symbol = getc(file_graph)) != EOF) {
         if (symbol == '\n') {
-            ribs ++;
+            numbers++;
         }
     }
-    ++ribs;
+    ++numbers;
     fclose(file_graph);
 
-    numbers = ribs * 2;
-    int array[numbers];
+    int Count = numbers;
+    numbers *= 2;
+    int *array = malloc(numbers * sizeof(int));
+    int *check = malloc(numbers * sizeof(int));    
+    int *temp = malloc(numbers * sizeof(int));
+
     file_graph = fopen("graph.txt", "r");
     for (int i = 0; i < numbers; i += 2) {
-        fscanf(file_graph, "%d-%d", &first, &second);
-        array[i] = first;
-        array[i+1] = second;
+        fscanf_s(file_graph, "%d-%d", &array[i], &array[i + 1]);
     }
     fclose(file_graph);
 
-    for(int i = 0; i < numbers; i += 2) {
-        if(array[i] == array[i+1]) {
-            Loop = 1;
-        }
-    }
+    result = fopen("result.txt", "w");
 
     for (int i = 0; i < numbers; i += 2) {
-        for ( int j = i + 2; j < numbers; j += 2){
-            if ((array[i] == array[j] && array[i+1] == array[j+1]) || (array[i] == array[j+1] && array[i+1] == array[j])){
-                Multiple = 1;
+        if (array[i] == array[i + 1]) {
+            printf("Graph is NOT a tree\n");
+            fprintf(result, "Graph is NOT a tree\n");
+            return;
+        }
+        for (int j = i + 2; j < numbers; j += 2) {
+            if ((array[i] == array[j] && array[i + 1] == array[j + 1]) 
+             || (array[i] == array[j + 1] && array[i + 1] == array[j])) {
+                printf("Graph is NOT a tree\n");
+                fprintf(result, "Graph is NOT a tree\n");
+                return;
             }
         }
     }
 
-    int check[numbers];
     check[size] = start = array[0];
     int h = array[1];
-    int Cycle = SearchCycle(numbers, array, a, x, size, check, start, h);
-
-    int Count = Sort(array, numbers, ribs);
-
-    printf("Count is %d\n", Count);
-    printf("Cycle is %d\n", Cycle);
-    printf("Loop is %d\n", Loop);
-    printf("Multiple is %d\n", Multiple);
-    result = fopen("result.txt", "w");
-    if (Loop == 0 && Multiple == 0 && Cycle != 1 && Count == 1){
-        printf("Graph is a tree\n");
-
-        fprintf(result, "graph {\n");
-        for (int i = 0; i < numbers; i=i+2) {
-            fprintf(result, "   %d -- %d;\n",array[i], array[i+1]);
-        }
-        fprintf(result, "}");
-    } else {
-    printf("Graph is NOT a tree\n");
-
-    fprintf(result, "The last graph was NOT a tree\n");
+    if (SearchCycle(numbers, array, size, check, start, h) == 1) {
+        printf("Graph is NOT a tree\n");
+        fprintf(result, "Graph is NOT a tree\n");
+        return;
     }
+    
+    Sort(array, numbers, temp);
+    for (int i = 1; i < numbers; i++) {
+        if (temp[i] == temp[i - 1]) {
+            --Count;
+        }
+    }
+    if (Count != 1) {
+        printf("Graph is NOT a tree\n");
+        fprintf(result, "Graph is NOT a tree\n");
+        return;
+    }
+    result = fopen("result.txt", "w");
+    fprintf(result, "Graph is NOT a tree\n");
     fclose(result);
-    return 0;
+
+    result = fopen("result.txt", "w");
+    printf("Graph is a tree\n");
+
+    fprintf(result, "graph {\n");
+    for (int i = 0; i < numbers; i = i + 2) {
+        fprintf(result, "   %d -- %d;\n", array[i], array[i + 1]);
+    }
+    fprintf(result, "}");
+
+    fclose(result);
 }
